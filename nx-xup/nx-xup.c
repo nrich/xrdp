9084afcdf68fbad550458c642e2cb227c8b6af23
Neil Richardson <nrich@ii.net>
2014-10-31 09:56:15 +0800

    Wrong field

--- nx-xup/nx-xup.c
+++ nx-xup/nx-xup.c
@@ -725,7 +725,7 @@ lib_mod_connect(struct mod *mod)
         char geometry[32];
 
 #ifdef EXTERNAL_X11RDP
-        send_disconnect(atoi(mod->display));
+        send_disconnect(atoi(mod->port));
 #else
         send_disconnect(mod->display);
 #endif
